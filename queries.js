afbfc89acda60687bf339e2dbd7809bdb5d8cd26
unknown <peter.ostertag99@gmail.com>
2023-07-31 14:47:06 -0500

    Created async function viewAllEmployees

--- queries.js
+++ queries.js
@@ -2,12 +2,52 @@ const db = require('./db');
 
 // Function to view all departments
 async function viewAllDepartments() {
-    const [rows] = await db.query('SELECT id, name FROM department');
+    const [rows] = await db.query(`
+        SELECT 
+            id, 
+            name 
+        FROM 
+            department
+    `);
     return rows;
 }
 
 // Function to view all rows
 async function viewAllRows() {
-    const [rows] = await db.query('SELECT id, title, salary, department_id, FROM role');
+    const [rows] = await db.query(`
+        SELECT 
+            id, 
+            title, 
+            salary, 
+            department_id, 
+        FROM 
+            role
+    `);
     return rows;
 }
+
+// Function to view all employees
+// Used LEFT JOIN to make reading for comprehensive
+async function viewAllEmployees() {
+    const [rows] = await db.query(`
+        SELECT
+            employee.id,
+            employee.first_name,
+            employee.last_name,
+            role.title AS job_title,
+            department.name AS department,
+            role.salary,
+            CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+        FROM
+            employee
+            LEFT JOIN role ON employee.role_id = role.id
+            LEFT JOIN department ON role.department_id = department.id
+            LEFT JOIN employee manager ON employee.manager_id = manager.id
+    `);
+    return rows;
+}
+
+module.exports = {
+    viewAllDepartments,
+    viewAllRows
+};
